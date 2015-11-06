21cface5b361d9a171442e1784fd42b1c747b7c0
kellyahergert <kellyahergert@Kitkat>
2015-11-06 09:38:02 -0700

    Email sending updates for robustness
    Updated email sender with exponential backoff on failure to send, also
    only connected to the session once to avoid 421 (too many concurrent
    sessions) exception.

--- AdoptAFamily/src/aaf/controller/BasicEmailSender.java
+++ AdoptAFamily/src/aaf/controller/BasicEmailSender.java
@@ -23,14 +23,31 @@ public class BasicEmailSender {
 	protected String myFromAddress = "";
 	protected EmailServerCredentials myServerCredentials;
 	protected Session mySession;
+	Transport myTransport;
 
+	/**
+	 * Main method is for testing purposes only
+	 * 
+	 * @param args
+	 * 			not used
+	 */
 	public static void main(String[] args)
 	{
 		String fromAddress = "kellytorkelson@gmail.com";
 		EmailServerCredentials creds = new EmailServerCredentials("smtp.comcast.net", 587, "jordhergert@comcast.net", "comcastPasswordGoesHere");
 
 		BasicEmailSender sender = new BasicEmailSender(fromAddress, creds);
-		sender.sendEmail("subject", "email text", "kellyahergert@gmail.com");
+		for (int i=1; i <= 5; i++)
+		{
+			System.out.println("====== Email " + i + " ============");
+			sender.sendEmail("subject " + i, "kitten email text " + i, "kellyahergert@gmail.com");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public BasicEmailSender(String fromAddress, EmailServerCredentials serverCredentials) {
@@ -58,6 +75,26 @@ public class BasicEmailSender {
 			};
 
 			mySession = Session.getInstance(properties, auth);
+			
+			// try to connect a maximum of 50 times, should only take one
+			for (int i=0; i < 50; i++)
+			{
+				try {
+
+					// save the transport object so each email is sent through the same
+					// connection, this prevents error 421 (too many concurrent sessions)
+					myTransport = mySession.getTransport();
+					
+					myTransport.connect(myServerCredentials.getHost(),
+					  myServerCredentials.getPort(),
+					  myServerCredentials.getUsername(),
+					  myServerCredentials.getPassword());
+					return;
+				} catch (MessagingException e) {
+					e.printStackTrace();
+				}
+			}
+
 		}
 		else
 		{
@@ -99,56 +136,87 @@ public class BasicEmailSender {
 	
 	public boolean sendEmailBool(String theSubject, String theText, String theToAddress, LinkedList<MimeBodyPart> attachments)
 	{
-		try
+		
+		long waitTime = 100;
+
+		// try sending the email a maximum of 100 times... it shouldn't take this many tries
+		for (int i=0; i < 100; i++)
 		{
-			System.out.println("Attempt sending message to " + theToAddress + "....");
-			
-			MimeMessage message = new MimeMessage(mySession);
-
-			message.setFrom(new InternetAddress(myFromAddress));
-
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
-					theToAddress));
-
-			message.setSubject(theSubject);
-
-			MimeBodyPart messageBodyPart = new MimeBodyPart();
-
-			messageBodyPart.setContent(theText, "text/html; charset=utf-8");
-
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(messageBodyPart);
-			
-			for (MimeBodyPart attachment : attachments)
+			try
 			{
-				multipart.addBodyPart(attachment);
+				System.out.println("Attempt sending message to " + theToAddress + "....");
+				
+				MimeMessage message = new MimeMessage(mySession);
+
+				message.setFrom(new InternetAddress(myFromAddress));
+
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+						theToAddress));
+
+				message.setSubject(theSubject);
+
+				MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+				messageBodyPart.setContent(theText, "text/html; charset=utf-8");
+
+				Multipart multipart = new MimeMultipart();
+				multipart.addBodyPart(messageBodyPart);
+				
+				for (MimeBodyPart attachment : attachments)
+				{
+					multipart.addBodyPart(attachment);
+				}
+				
+				message.setContent(multipart);
+
+				if (myServerCredentials.getUsername() != "" && myServerCredentials.getPassword() != "")
+				{
+					// uncomment this block to inject exceptions for testing purposes
+//					int rand = (int) (Math.random() * 10);
+//					System.out.println(rand);
+//					if (rand % 2 == 0)
+//					{
+//						throw new MessagingException("Test Msg Exception");
+//					}
+					
+					myTransport.sendMessage(message, message.getAllRecipients());
+				}
+				else
+				{
+					Transport.send(message);
+				}
+				
+				System.out.println("---Sent message successfully to " + theToAddress + "....");
+				return true;
+			}
+			catch (MessagingException mex)
+			{			
+				System.out.println(mex.getMessage());
 			}
 			
-			message.setContent(multipart);
-			
-			if (myServerCredentials.getUsername() != "" && myServerCredentials.getPassword() != "")
+			// exponential back-off if email wasn't sent successfully
+			try 
 			{
-				Transport transport = mySession.getTransport();
-
-				transport.connect(myServerCredentials.getHost(),
-						          myServerCredentials.getPort(),
-						          myServerCredentials.getUsername(),
-						          myServerCredentials.getPassword());
-				transport.sendMessage(message, message.getAllRecipients());
+				Thread.sleep(waitTime);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			
+			final int FIVE_MINUTES = 300000;
+			if (waitTime <= FIVE_MINUTES)
+			{
+				waitTime *= 2;
 			}
 			else
 			{
-				Transport.send(message);
-			}
+				// reset the wait time so it doesn't get too high
+				waitTime = 100;
+			}		
 			
-			System.out.println("---Sent message successfully to " + theToAddress + "....");
-			return true;
 		}
-		catch (MessagingException mex)
-		{			
-			System.out.println(mex.getMessage());
-			return false;
-		}
+		return false;
 	}
 	
 }
